774e8e94fda57cb5752e5b597b0e3d72a46d171d
Turner Luke <70869027+turnerluke@users.noreply.github.com>
2023-06-13 12:32:13 -0500

    Update message.py

--- ziki_helpers/slack/message.py
+++ ziki_helpers/slack/message.py
@@ -6,7 +6,9 @@ from slack_sdk import WebClient
 client = WebClient(token=os.getenv('SLACK_TOKEN'))
 
 
-def send_message_to_slack_channel(channel_name: str, message: str):
+def send_message_to_slack_channel(message: str, channel_name: str = None) -> None:
+    if channel_name is None:
+        channel_name = os.getenv('SLACK_CHANNEL')
     # Call the conversations.list method using the WebClient
     client.chat_postMessage(
         channel="#"+channel_name,
