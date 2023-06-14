6a793635d77cef5834e0a0d9218d01bc961e8401
turner <turnermluke@gmail.com>
2023-06-14 17:28:50 -0500

    Updated to handle dependencies.

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from setuptools import setup, find_packages
+
+# Get requirements from requirements.txt
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='ziki_helpers',
     version='1.0',
@@ -6,7 +11,14 @@ setup(
     author_email='turnermluke@gmail.com',
     description='Helper functions shared between multiple ZIKI projects.',
     url='https://github.com/turnerluke/ziki-helpers',
+    # Commented as it's more readable to import from the root
+    # ie: from ziki_helpers.service.service import fxn
+
     # packages=find_packages('ziki_helpers'),
     # package_dir={'': 'ziki_helpers'},
     # test_suite='tests',
+
+    # Install dependencies
+    install_requires=requirements,
+
 )
