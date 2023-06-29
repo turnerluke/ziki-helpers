031cf4e029aef6a015a622a256af57a57d0b2725
turner <turnermluke@gmail.com>
2023-06-29 15:40:17 -0500

    Added optional thread timestamp.

--- ziki_helpers/slack/message.py
+++ ziki_helpers/slack/message.py
@@ -6,7 +6,7 @@ from slack_sdk import WebClient
 client = WebClient(token=os.getenv('SLACK_TOKEN'))
 
 
-def send_message_to_slack_channel(message: str, channel_name: str = None) -> None:
+def send_message_to_slack_channel(message: str, channel_name: str = None, thread_ts: str = None) -> None:
     if channel_name is None:
         channel_name = os.getenv('SLACK_CHANNEL')
     # Call the conversations.list method using the WebClient
@@ -14,4 +14,5 @@ def send_message_to_slack_channel(message: str, channel_name: str = None) -> Non
         channel="#"+channel_name,
         text=message,
         link_names=True,
+        thread_ts=thread_ts
     )
