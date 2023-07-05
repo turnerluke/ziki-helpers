17a02bad49f48e0636e265a0251114dc836e6a1e
turner <turnermluke@gmail.com>
2023-07-05 15:35:34 -0500

    Returns the timestamp of a sent message.

--- ziki_helpers/slack/message.py
+++ ziki_helpers/slack/message.py
@@ -6,13 +6,14 @@ from slack_sdk import WebClient
 client = WebClient(token=os.getenv('SLACK_TOKEN'))
 
 
-def send_message_to_slack_channel(message: str, channel_name: str = None, thread_ts: str = None) -> None:
+def send_message_to_slack_channel(message: str, channel_name: str = None, thread_ts: str = None) -> str:
     if channel_name is None:
         channel_name = os.getenv('SLACK_CHANNEL')
     # Call the conversations.list method using the WebClient
-    client.chat_postMessage(
+    result = client.chat_postMessage(
         channel="#"+channel_name,
         text=message,
         link_names=True,
         thread_ts=thread_ts
     )
+    return result['ts']
