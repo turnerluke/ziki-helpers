cd5a8b8fba02ef8f7106a609fc5c6898f99c9c5a
turner <turnermluke@gmail.com>
2023-07-10 12:26:40 -0500

    Added gmail.send scope to google credentials.

--- ziki_helpers/gcloud/auth.py
+++ ziki_helpers/gcloud/auth.py
@@ -87,6 +87,7 @@ def get_authenticated_google_credentials():
         "https://www.googleapis.com/auth/drive",
         "https://www.googleapis.com/auth/presentations",
         "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/gmail.send"
     ]
 
     auth_creds_file_name = 'google-auth-credential-sample.json'
