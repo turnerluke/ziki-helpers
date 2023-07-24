fd230312d2cc1295aa444e3396f1df640bf670d7
turner <turnermluke@gmail.com>
2023-07-24 11:22:35 -0500

    Added 2 tests.

--- tests/test_toast_connector.py
+++ tests/test_toast_connector.py
@@ -1,6 +1,14 @@
-from ziki_helpers.toast_api.connector import is_iso_datetime, is_camel_case, is_snake_case, snake_case_to_camel_case
+from ziki_helpers.toast_api.connector import ToastConnector, is_iso_datetime, is_camel_case, is_snake_case, snake_case_to_camel_case
+from ziki_helpers.aws.dynamodb import get_entire_table
 
+import pandas as pd
 
+conn = ToastConnector()
+
+locations = get_entire_table('locations')
+soco = [loc for loc in locations if
+        loc['info'][0]['verboseName'] == 'South Congress'][0]
+soco_guid = soco['guid']
 
 
 def test_string_helpers():
@@ -17,3 +25,21 @@ def test_string_helpers():
     assert not is_snake_case('testString')
 
     assert snake_case_to_camel_case('test_string') == 'testString'
+
+
+def test_get_orders_by_business_date():
+    data = conn.get_orders_by_business_date(20220201, soco_guid)
+    assert data, 'No data returned'
+    assert len(data) > 10, 'Data returned is too small'
+    df = pd.DataFrame(data)
+    assert 'guid' in df.columns, 'guid column not in data'
+
+
+def test_get_labor_by_business_date():
+
+    data = conn.get_labor_by_business_date(20220301, soco_guid)
+
+    assert data, 'No data returned'
+    assert len(data) >= 1, 'Data returned is too small'
+    df = pd.DataFrame(data)
+    assert 'guid' in df.columns, 'guid column not in data'
